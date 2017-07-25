c95392e8e781e68654ddb76a1109e15334e9375c
Levinet Nicolai <levinet.nicolai@gmail.com>
2017-07-25 12:48:53 +0300

    Proiectul generat in AtmelStudio-microcontrolerul Atmega2560

---
+++ Winder/Winder/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Winder.cpp
+ *
+ * Created: 7/25/2017 12:47:12 PM
+ * Author : CNTS04
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
